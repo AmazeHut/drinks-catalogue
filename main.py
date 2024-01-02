215f5607b3a49bde093d6529ac6a76ea218540ab
Mahad <56235065+Mahad-lab@users.noreply.github.com>
2024-01-02 18:50:23 +0500

    finished

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request 
 import utils
 
 drinks = utils.load_drinks()
@@ -9,15 +9,73 @@ allergy_info = utils.get_allergy_info(drinks)
 
 app = Flask(__name__)
 
+nav_menu = {
+    "Category": categories,
+    "Flavor": flavor_profiles,
+    "Diet": dietary_preferences,
+    "Allergy": allergy_info,
+}
+
+print(nav_menu)
+
 
 @app.route('/')
-def index():
-    return render_template('index.html')
-
-
 @app.route('/home')
 def home():
-    return render_template('home.html', drinks=drinks)
+    return render_template('home.html', drinks=drinks, menu=nav_menu)
+
+@app.route('/search')
+def search():
+    search_term = request.args.get('q').lower()
+    selected_drinks = []
+    for drink in drinks:
+        for elem in drink.values():
+            if search_term in str(elem).lower():
+                if drink not in selected_drinks:
+                    selected_drinks.append(drink)
+            
+    return render_template('home.html', drinks=selected_drinks, menu=nav_menu)
 
 
-app.run(host='0.0.0.0', port=81)
+@app.route('/category/<category>')
+def category(category):
+    if category in categories:
+        selected_drinks = [
+            drink for drink in drinks if drink['category'].lower() == category.lower()]
+        return render_template('home.html', drinks=selected_drinks, menu=nav_menu)
+    else:
+        return "Category not found"
+
+
+@app.route('/flavor/<flavor_profile>')
+def flavor_profile(flavor_profile):
+    if flavor_profile in flavor_profiles:
+        selected_drinks = [
+            drink for drink in drinks if drink['flavor_profile'].lower() == flavor_profile.lower()]
+        return render_template('home.html', drinks=selected_drinks, menu=nav_menu)
+    else:
+        return "Flavor profile not found"
+
+
+@app.route('/diet/<dietary_preference>')
+def dietary_preference(dietary_preference):
+    if dietary_preference in dietary_preferences:
+        selected_drinks = [
+            drink for drink in drinks if dietary_preference in list(map(str.lower, drink['dietary_preferences']))]
+        return render_template('home.html', drinks=selected_drinks, menu=nav_menu)
+    else:
+        return "Dietary preference not found"
+
+
+@app.route('/allergy/<allergy>')
+def allergy(allergy):
+    if allergy in allergy_info:
+        selected_drinks = [
+            drink for drink in drinks if allergy in list(map(str.lower, drink['allergy_info']))]
+        return render_template('home.html', drinks=selected_drinks, menu=nav_menu)
+    else:
+        return "Allergy not found"
+
+
+if __name__ == '__main__':
+    app.run(port=5000)
--- utils.py
+++ utils.py
@@ -11,25 +11,33 @@ def get_categories(drinks):
     categories = []
     for drink in drinks:
         if drink["category"] not in categories:
-            categories.append(drink["category"])
+            categories.append(drink["category"].lower())
     return categories
 
 
 def get_flavor_profiles(drinks):
     flavor_profiles = []
     for drink in drinks:
-        flavor_profiles = []
         if drink["flavor_profile"] not in flavor_profiles:
-            flavor_profiles.append(drink["flavor_profile"])
+            flavor_profiles.append(drink["flavor_profile"].lower())
     return flavor_profiles
+
+
+def get_ingredients(drinks):
+    ingredients = []
+    for drink in drinks:
+        for ingredient in drink["ingredients"]:
+            if ingredient.lower() not in ingredients:
+                ingredients.append(ingredient.lower())
+    return ingredients
 
 
 def get_dietary_preferences(drinks):
     dietary_preferences = []
     for drink in drinks:
         for preference in drink["dietary_preferences"]:
-            if preference not in dietary_preferences:
-                dietary_preferences.append(preference)
+            if preference.lower() not in dietary_preferences:
+                dietary_preferences.append(preference.lower())
     return dietary_preferences
 
 
@@ -37,6 +45,7 @@ def get_allergy_info(drinks):
     allergy_info = []
     for drink in drinks:
         for allergy in drink["allergy_info"]:
-            if allergy not in allergy_info:
-                allergy_info.append(allergy)
+            if allergy.lower() not in allergy_info:
+                print(allergy_info)
+                allergy_info.append(allergy.lower())
     return allergy_info
